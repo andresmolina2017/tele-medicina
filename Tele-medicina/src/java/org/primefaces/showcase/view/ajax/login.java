275e3e116f76cbf07d04c126bbd9d9e485f0ed14
andres fonseca <carlosandresbautista@unipanamericana.edu.co>
2016-04-28 15:04:19 -0500

    pagina de recuperacion de clave
    se adiciona pagina diseño de recuperacion de clave

--- Tele-medicina/src/java/org/primefaces/showcase/view/ajax/login.java
+++ Tele-medicina/src/java/org/primefaces/showcase/view/ajax/login.java
@@ -1,6 +1,7 @@
 
 package org.primefaces.showcase.view.ajax;
  
+import static com.sun.javafx.logging.PulseLogger.addMessage;
 import javafx.event.ActionEvent;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -13,7 +14,17 @@ import javax.faces.context.FacesContext;
 public class login {
     private String usuario;
     private String clave;
+    private String email;
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
     public String getUsuario() {
         return usuario;
     }
@@ -46,5 +57,14 @@ public class login {
      public String Index(){
          return "Index.xhtml";
      }
+     public String Conocenos(){
+         return "Conocenos.xhtml";
+     }
+     public String Recuperacion(){
+         return "RecuperacionClave.xhtml";
+     }
+     public void Email(ActionEvent actionEvent) {
+        addMessage(" Mensaje envia a correo electronico!!");
     }
+}
 
